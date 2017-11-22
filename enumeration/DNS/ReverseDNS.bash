db4b88a1187eb851d698119ca5305200af9c4bb6
Jase <magicsbro@gmail.com>
2017-11-22 15:45:51 +1100

    Update ReverseDNS.bash

--- enumeration/DNS/ReverseDNS.bash
+++ enumeration/DNS/ReverseDNS.bash
@@ -15,3 +15,24 @@ done
 # The reason for seq 72 to 91:
 # The forward DNS bruteforce revealed hostnames between these two IPs (38.100.193.72 and 38.100.193.91)...
 # Therefore, to stay in bounds I chose to stick within this range.
+
+# So executed host on the mockup domain: megacorpone.com,
+# the returned the following IP: 38.100.193.76
+# Therfore, used the above IP to run a reverse DNS lookup - ran from 38.100.93.76 to 38.100.193.91 range (though can be set higher obviously),
+# This returned the following:
+#
+# 66.193.100.38.in-addr.arpa domain name pointer syslog.megacorpone.com
+# 69.193.100.38.in-addr.arpa domain name pointer beta.megacorpone.com
+# 70.193.100.38.in-addr.arpa domain name pointer ns1.megacorpone.com
+# 72.193.100.38.in-addr.arpa domain name pointer admin.megacorpone.com
+# 73.193.100.38.in-addr.arpa domain name pointer mail2.megacorpone.com
+# 76.193.100.38.in-addr.arpa domain name pointer www.megacorpone.com
+# 77.193.100.38.in-addr.arpa domain name pointer vpn.megacorpone.com
+# 80.193.100.38.in-addr.arpa domain name pointer ns2.megacorpone.com
+# 84.193.100.38.in-addr.arpa domain name pointer mail.megacorpone.com
+# 85.193.100.38.in-addr.arpa domain name pointer snmp.megacorpone.com
+# 89.193.100.38.in-addr.arpa domain name pointer siem.megacorpone.com
+# 90.193.100.38.in-addr.arpa domain name pointer ns3.megacorpone.com
+# 91.193.100.38.in-addr.arpa domain name pointer router.megacorpone.com
+#
+# Now we have more targets. 
