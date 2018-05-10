63fa2e8ef5ebd77c9126f49c480c38b64d5031e7
Jase <magicsbro@gmail.com>
2018-05-10 10:55:17 +1000

    Update host-sweep.sh

--- enumeration/DNS/host-sweep.sh
+++ enumeration/DNS/host-sweep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Run only after altering /etc/resolv.conf to point to the internal network's DNS server (port scan for tcp/53)
+# Run only after altering /etc/resolv.conf to point to the internal network's DNS server (port scan subnet for tcp/53)
 for ip in $(cat lab-targets.txt); do
 host ${ip}
 done
